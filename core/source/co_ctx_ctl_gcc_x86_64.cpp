09313b4c67e75d5bde8a1183629587a436798da5
skyfire <skyfireitdiy@hotmail.com>
2021-10-30 04:25:32 +0800

    增加信号上下文

--- core/source/co_ctx_ctl_gcc_x86_64.cpp
+++ core/source/co_ctx_ctl_gcc_x86_64.cpp
@@ -19,6 +19,38 @@ static constexpr int                  reg_index_RBP__   = 3;
 static constexpr int                  reg_index_MXCSR__ = 9;
 static constexpr int                  reg_index_FCW__   = 10;
 
+struct sigcontext_64
+{
+    unsigned long long r8;           //0*8
+    unsigned long long r9;           //1*8
+    unsigned long long r10;          //2*8
+    unsigned long long r11;          //3*8
+    unsigned long long r12;          //4*8
+    unsigned long long r13;          //5*8
+    unsigned long long r14;          //6*8
+    unsigned long long r15;          //7*8
+    unsigned long long di;           //8*8
+    unsigned long long si;           //9*8
+    unsigned long long bp;           //10*8
+    unsigned long long bx;           //11*8
+    unsigned long long dx;           //12*8
+    unsigned long long ax;           //13*8
+    unsigned long long cx;           //14*8
+    unsigned long long sp;           //15*8
+    unsigned long long ip;           //16*8
+    unsigned long long flags;        //17*8
+    unsigned short     cs;           //18*8
+    unsigned short     gs;           //18*8+2
+    unsigned short     fs;           //18*8+4
+    unsigned short     __pad0;       //18*8+6
+    unsigned long long err;          //19*8
+    unsigned long long trapno;       //20*8
+    unsigned long long oldmask;      //21*8
+    unsigned long long cr2;          //22*8
+    unsigned long long fpstate;      //23*8
+    unsigned long long reserved1[8]; //24*8
+};
+
 co_byte* get_rsp(co_ctx* ctx)
 {
     return ctx->regs()[reg_index_RSP__];
