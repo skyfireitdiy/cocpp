6ee2491f79183d2e5f352aa4457455ae81f859c5
skyfire <skyfireitdiy@hotmail.com>
2021-11-24 03:01:26 +0800

    修改可调度对象类命名

--- core/include/co_scheduler.h
+++ core/include/co_scheduler.h
@@ -7,27 +7,27 @@
 
 CO_NAMESPACE_BEGIN
 
-class schedulable
+class schedulable_obj
 {
 public:
     virtual int  priority() const           = 0;
     virtual void set_priority(int priority) = 0;
     virtual bool can_schedule() const       = 0;
 
-    virtual ~schedulable() = default;
+    virtual ~schedulable_obj() = default;
 };
 
 class co_scheduler : private co_nocopy
 {
 public:
-    virtual void                    add_obj(schedulable* obj)                  = 0;
-    virtual void                    remove_obj(schedulable* obj)               = 0;
-    virtual schedulable*            choose_obj()                               = 0;
-    virtual std::list<schedulable*> all_obj() const                            = 0;
-    virtual size_t                  count() const                              = 0;
-    virtual schedulable*            current_obj() const                        = 0;
-    virtual bool                    can_schedule() const                       = 0;
-    virtual void                    change_priority(int old, schedulable* obj) = 0;
+    virtual void                        add_obj(schedulable_obj* obj)                  = 0;
+    virtual void                        remove_obj(schedulable_obj* obj)               = 0;
+    virtual schedulable_obj*            choose_obj()                                   = 0;
+    virtual std::list<schedulable_obj*> all_obj() const                                = 0;
+    virtual size_t                      count() const                                  = 0;
+    virtual schedulable_obj*            current_obj() const                            = 0;
+    virtual bool                        can_schedule() const                           = 0;
+    virtual void                        change_priority(int old, schedulable_obj* obj) = 0;
 
     virtual ~co_scheduler() = default;
 };
--- scheduler/include/co_o1_scheduler.h
+++ scheduler/include/co_o1_scheduler.h
@@ -13,21 +13,21 @@ CO_NAMESPACE_BEGIN
 class co_o1_scheduler : public co_scheduler
 {
 private:
-    std::vector<std::list<schedulable*>> all_obj__;
-    mutable std::mutex                   mu_all_obj__;
-    schedulable*                         curr_obj__ { nullptr };
+    std::vector<std::list<schedulable_obj*>> all_obj__;
+    mutable std::mutex                       mu_all_obj__;
+    schedulable_obj*                         curr_obj__ { nullptr };
 
     co_o1_scheduler();
 
 public:
-    void                    add_obj(schedulable* obj) override;
-    void                    remove_obj(schedulable* obj) override;
-    schedulable*            choose_obj() override;
-    std::list<schedulable*> all_obj() const override;
-    size_t                  count() const override;
-    schedulable*            current_obj() const override;
-    bool                    can_schedule() const override;
-    void                    change_priority(int old, schedulable* obj) override;
+    void                        add_obj(schedulable_obj* obj) override;
+    void                        remove_obj(schedulable_obj* obj) override;
+    schedulable_obj*            choose_obj() override;
+    std::list<schedulable_obj*> all_obj() const override;
+    size_t                      count() const override;
+    schedulable_obj*            current_obj() const override;
+    bool                        can_schedule() const override;
+    void                        change_priority(int old, schedulable_obj* obj) override;
 
     friend class co_o1_scheduler_factory;
 };
