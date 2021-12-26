a0e1114d9a9fdd1557b33eb40ec4100c8c9a7d6a
skyfire <skyfireitdiy@hotmail.com>
2021-12-26 13:02:48 +0800

    删除u多余文件

--- build_in_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./build_docker_image.sh
-echo docker run -it --rm -u $(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd) cocpp_build:lastest 
-docker run -it --rm -u $(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd) cocpp_build:lastest 
--- docker/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-xmake f --mode=release
-xmake
