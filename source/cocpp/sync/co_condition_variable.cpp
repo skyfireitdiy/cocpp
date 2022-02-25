f373043f40a75ca686ec363153d2bf73cacb9150
skyfire <skyfireitdiy@hotmail.com>
2022-02-25 23:19:38 +0800

    Implement condition variable (error)

--- source/cocpp/sync/co_condition_variable.cpp
+++ source/cocpp/sync/co_condition_variable.cpp
@@ -12,4 +12,27 @@ void notify_all_at_co_exit(co_condition_variable& cond)
     });
 }
 
+void co_condition_variable::notify_all()
+{
+    std::scoped_lock lk(cv_lock__);
+    for (auto ctx : waiters__)
+    {
+        ctx->leave_wait_resource_state();
+    }
+    waiters__.clear();
+}
+
+void co_condition_variable::notify_one()
+{
+    std::scoped_lock lk(cv_lock__);
+    if (waiters__.empty())
+    {
+        cv_lock__.unlock();
+        return;
+    }
+    auto ctx = waiters__.front();
+    waiters__.pop_front();
+    ctx->leave_wait_resource_state();
+}
+
 CO_NAMESPACE_END
