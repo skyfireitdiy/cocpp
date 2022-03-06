b43b6e033ea8f319b31cda5c493dd7ff5aba22e9
skyfire <skyfireitdiy@hotmail.com>
2022-03-06 10:37:29 +0800

    Restoring memory sequence

--- source/cocpp/sync/co_spinlock.cpp
+++ source/cocpp/sync/co_spinlock.cpp
@@ -2,13 +2,14 @@
 #include "cocpp/core/co_define.h"
 #include "cocpp/core/co_env.h"
 #include "cocpp/core/co_manager.h"
+#include <atomic>
 #include <thread>
 
 CO_NAMESPACE_BEGIN
 
 void co_spinlock::lock()
 {
-    while (locked__.test_and_set())
+    while (locked__.test_and_set(std::memory_order::acquire))
     {
         co_manager::instance()->current_env()->schedule_switch();
     }
@@ -16,12 +17,12 @@ void co_spinlock::lock()
 
 bool co_spinlock::try_lock()
 {
-    return !locked__.test_and_set();
+    return !locked__.test_and_set(std::memory_order::acquire);
 }
 
 void co_spinlock::unlock()
 {
-    locked__.clear();
+    locked__.clear(std::memory_order::release);
 }
 
 CO_NAMESPACE_END
