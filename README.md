ca5ccb6e83311b10a7ae391a8931792456a96550
skyfire <skyfireitdiy@hotmail.com>
2022-07-04 11:21:11 +0000

    修正readme

--- README.md
+++ README.md
@@ -74,6 +74,40 @@ int main()
 
 	方便的协程局部存储，让每个协程拥有自己的私有数据。
 
+- 支持 pipeline 的并行数据处理模式
+
+```cpp
+#include "cocpp/cocpp.h"
+#include <iostream>
+
+using namespace cocpp;
+using namespace std;
+
+int main()
+{
+    int source = 0;
+    auto ch = co_pipeline<int>([&source]() -> std::optional<int> { // 生成[0, 1000000) 序列
+                  if (source < 1000000)
+                  {
+                      return source++;
+                  }
+                  return std::nullopt;
+              })
+              | pipeline::take(2000) // 取前 2000 项
+              | pipeline::skip(1000) // 跳过前 1000 项 （相当于取[1000, 1999)）
+              | pipeline::fork(10, [](int n) -> int { // 每个数字乘以2 ，10个协程同时计算
+                    return n * 2;
+                })
+              | pipeline::filter([](int n) { return n % 3 == 0; })  // 过滤出 3 的倍数
+              | pipeline::reduce([](int n, int m) { return n + m; }, 0) // 求和
+              | pipeline::chan(); // 转换为 channel
+
+    for (auto t : ch)
+    {
+        cout << t << endl;
+    }
+}
+```
 
 ## 编译安装
 
--- example/pipeline.cpp
+++ example/pipeline.cpp
@@ -1,24 +1,27 @@
 #include "cocpp/cocpp.h"
-#include <chrono>
 #include <iostream>
-#include <vector>
 
 using namespace cocpp;
 using namespace std;
 
 int main()
 {
-    auto ch = std::list({ 0, 1, 2, 3, 4, 5 })
-              | pipeline::stream(1)
-              | [](int a) {
-                    this_co::sleep_for(10ms);
-                    return a + 1;
-                }
-              | [](int a) {
-                    this_co::sleep_for(10ms);
-                    return a * 2;
-                }
-              | pipeline::filter([](int n) { return n % 3 == 0; }) | pipeline::reduce([](int a, int b) { return a * b; }, 1) | pipeline::to<vector<int>>();
+    int source = 0;
+    auto ch = co_pipeline<int>([&source]() -> std::optional<int> { // 生成[0, 1000000) 序列
+                  if (source < 1000000)
+                  {
+                      return source++;
+                  }
+                  return std::nullopt;
+              })
+              | pipeline::take(2000) // 取前 2000 项
+              | pipeline::skip(1000) // 跳过前 1000 项 （相当于取[1000, 1999)）
+              | pipeline::fork(10, [](int n) -> int { // 每个数字乘以2 ，10个协程同时计算
+                    return n * 2;
+                })
+              | pipeline::filter([](int n) { return n % 3 == 0; })  // 过滤出 3 的倍数
+              | pipeline::reduce([](int n, int m) { return n + m; }, 0) // 求和
+              | pipeline::chan(); // 转换为 channel
 
     for (auto t : ch)
     {
