f5ac6f1a6c1a6498df09ad29b82b246c45556666
skyfire <skyfireitdiy@hotmail.com>
2024-05-01 00:17:12 +0800

    补充测试用例

--- test/file.cpp
+++ test/file.cpp
@@ -3,10 +3,118 @@
 
 using namespace cocpp;
 
-TEST(file, open_file)
+bool file_exists(const char *filename)
+{
+    struct stat st;
+    return stat(filename, &st) == 0;
+}
+
+int get_file_mode(const char *filename)
+{
+    struct stat st;
+    stat(filename, &st);
+    return st.st_mode;
+}
+
+TEST(file, open_null)
 {
     co_file f;
-    int ret = f.open("/dev/null", O_RDONLY, 0);
+    int ret = f.open("/dev/null", O_RDONLY, 0644);
     EXPECT_EQ(ret, 0);
     f.close();
+}
+
+TEST(file, open_disk)
+{
+    co_file f;
+    int ret = f.open("test_open_disk_file", O_RDWR | O_CREAT, 0644);
+    EXPECT_EQ(ret, 0);
+    f.close();
+    // 判断文件是否存在
+    EXPECT_TRUE(file_exists("test_open_disk_file"));
+    remove("test_open_disk_file");
+}
+
+TEST(file, open_invalid)
+{
+    co_file f;
+    int ret = f.open("invalid_file", O_RDONLY, 0644);
+    EXPECT_EQ(ret, -1);
+    EXPECT_FALSE(file_exists("invalid_file"));
+}
+
+TEST(file, read_write)
+{
+    co_file f;
+    int ret = f.open("test_write_file", O_RDWR | O_CREAT, 0644);
+    EXPECT_EQ(ret, 0);
+    ret = f.write("hello", 5);
+    EXPECT_EQ(ret, 5);
+    f.close();
+
+    EXPECT_TRUE(file_exists("test_write_file"));
+
+    co_file f2;
+    ret = f2.open("test_write_file", O_RDONLY, 0644);
+    EXPECT_EQ(ret, 0);
+    char buf[10] = {0};
+    ret = f2.read(buf, 10);
+    EXPECT_EQ(ret, 5);
+    EXPECT_STREQ(buf, "hello");
+    f2.close();
+
+    remove("test_write_file");
+}
+
+TEST(file, lseek)
+{
+    co_file f;
+    int ret = f.open("test_lseek_file", O_RDWR | O_CREAT, 0644);
+    EXPECT_EQ(ret, 0);
+    ret = f.write("hello", 5);
+    EXPECT_EQ(ret, 5);
+    f.close();
+
+    EXPECT_TRUE(file_exists("test_lseek_file"));
+
+    co_file f2;
+    ret = f2.open("test_lseek_file", O_RDONLY, 0644);
+    EXPECT_EQ(ret, 0);
+    ret = f2.lseek(2, SEEK_SET);
+    EXPECT_EQ(ret, 2);
+    char buf[10] = {0};
+    ret = f2.read(buf, 10);
+    EXPECT_EQ(ret, 3);
+    EXPECT_STREQ(buf, "llo");
+    f2.close();
+    remove("test_lseek_file");
+}
+
+TEST(file, fchmod)
+{
+    co_file f;
+    int ret = f.open("test_chmod_file", O_RDWR | O_CREAT, 0644);
+    EXPECT_EQ(ret, 0);
+
+    ret = f.fchmod(0755);
+    EXPECT_EQ(ret, 0);
+    f.close();
+
+    int mode = get_file_mode("test_chmod_file");
+    EXPECT_EQ(mode & ~S_IFMT, 0755);
+
+    remove("test_chmod_file");
+}
+
+TEST(file, fchown)
+{
+    co_file f;
+    int ret = f.open("test_chown_file", O_RDWR | O_CREAT, 0644);
+    EXPECT_EQ(ret, 0);
+
+    ret = f.fchown(1000, 1000);
+    EXPECT_EQ(ret, 0);
+    f.close();
+
+    // todo
 }
